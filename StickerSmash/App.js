ce3c0a7a2785fc01e109e11ca352cfbd871d6130
BrunoOller <brunobrunelli4@gmail.com>
2024-03-13 12:26:25 -0300

    Adição de variáveis de estado.

--- StickerSmash/App.js
+++ StickerSmash/App.js
@@ -1,19 +1,53 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Image } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import ImageViewer from './src/components/ImageViewer';
+import Button from './src/components/Button';
+import * as ImagePicker from 'expo-image-picker';
 
 const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [showAppOptions, setShowAppOptions] = useState(false);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+      setShowAppOptions(true);
+    } else {
+      alert("Você não selecionou uma imagem");
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={PlaceholderImage} />
+        <ImageViewer
+          placeholderImageSource={PlaceholderImage}
+          selectedImage={selectedImage}
+        />
       </View>
-      <View style = {styles.footerContainer} >
-        <Button label = "Escolher foto"/>
-        <Button label = "Usar esta foto"/>
+        { showAppOptions ? (
+          <view />
+        ) : (
+      <View style={styles.footerContainer}>
+        <Button
+          label="Escolher Foto"
+          theme="primary"
+          onPress={pickImageAsync}
+        />
+        <Button
+          label="Usar esta Foto"
+          onPress={() => setShowAppOptions(true)}
+        />
       </View>
+        )}
+
       <StatusBar style="auto" />
     </View>
   );
@@ -22,7 +56,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
+    backgroundColor: '#25292e',
     alignItems: 'center',
   },
   imageContainer: {
@@ -34,4 +68,3 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   }
 });
-
